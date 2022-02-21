1d5b3561133088396693e10b69f6549ad8b4038d
Winfried Loetzsch <winfried.loetzsch@merantix.com>
2022-02-21 12:46:39 +0100

    Add test for Hub driver

---
+++ test/conftest.py
@@ -0,0 +1,17 @@
+"""This module defines specific fixtures for unit tests. Shared fixtures are defined in shared_fixtures.py.
+
+###################################
+Please do not import from this file.
+###################################
+
+Not importing from conftest is a best practice described in the note here:
+https://pytest.org/en/6.2.x/writing_plugins.html#conftest-py-local-per-directory-plugins
+"""
+
+import pytest
+
+
+@pytest.fixture()
+def unit_test_fixture() -> str:
+    """Fixture that is only used in the unit test scope."""
+    return "unit test string"
