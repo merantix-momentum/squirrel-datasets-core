57f1a91e7b9c7d91594463e1e60e42dae02ee513
Winfried Loetzsch <winfried.loetzsch@merantix.com>
2022-02-24 18:47:44 +0100

    Add spark preprocessing + Process IterStream

---
+++ src/squirrel_datasets_core/spark/__init__.py
@@ -0,0 +1,4 @@
+from squirrel_datasets_core.spark.setup_spark import get_spark, stop_spark
+from squirrel_datasets_core.spark.dummy_df import get_dummy_df
+
+__all__ = ["get_spark", "stop_spark", "get_dummy_df"]
