1b2366d8c37bddc8089055c0cbbae75f12cc85a5
Winfried Loetzsch <winfried.loetzsch@merantix.com>
2022-03-11 17:51:47 +0100

    Add docstring to save_composable_to_shards

--- src/squirrel_datasets_core/preprocessing/save_shards.py
+++ src/squirrel_datasets_core/preprocessing/save_shards.py
@@ -33,15 +33,28 @@ def save_composable_to_shards(
     src_it: Composable,
     session: SparkSession,
     out_url: str,
-    num_samples: int,
     num_shards: int,
+    num_samples: int = None,
     hooks: Optional[List[Callable[[Dict], Dict]]] = None,
     save_catalog: Optional[bool] = False,
     catalog_identifier: Optional[str] = "",
     catalog_version: Optional[int] = 1,
     catalog_output_url: Optional[str] = "",
 ) -> None:
-    """Save single iterstream to messagepack."""
+    """Save single iterstream to messagepack.
+    
+    Args:
+        src_it (Composable): Composable to fetch data from.
+        session (SparkSession): Spark session to use for processing.
+        out_url (str): Url where to store data.
+        num_shards (int): Number of msgpack shards to create.
+        num_samples (int): Optionally store only n samples from the composable.
+        hooks (Optional[List[Callable[[Dict], Dict]]]): Methods to map to the samples before creating shards.
+        save_catalog (Optional[bool]): Optionally save the created dataset to a squirrel catalog.
+        catalog_identifier (Optional[str]): Dataset identifier for squirrel catalog.
+        catalog_version (Optional[int]): Dataset version for squirrel catalog.
+        catalog_output_url (Optional[str]): Output url where to store the squirrel catalog.
+    """
     if num_samples is not None:
         src_it = src_it.take(num_samples)
 
