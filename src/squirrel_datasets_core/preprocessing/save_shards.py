a6f17f8e1a8f9cb6de6ac244d7fe233c50449424
Winfried Lötzsch <winfried.loetzsch@merantix.com>
2022-03-14 16:59:42 +0100

    Refactor Spark Processing (#4)
    * Add spark preprocessing + Process IterStream
    
    * Fix bug wrt number of shards. Store composable
    
    * Do not set key explicitly for saving shards
    
    * Add ability to specify number of master cores
    
    * Use repartition instead of coalesce
    
    * Add docstring to save_composable_to_shards

---
+++ src/squirrel_datasets_core/preprocessing/save_shards.py
@@ -0,0 +1,103 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from functools import partial
+from typing import Callable, Dict, List, Optional, TYPE_CHECKING
+
+from pyspark.sql import SparkSession
+from squirrel.catalog import Catalog, Source
+from squirrel.serialization import MessagepackSerializer
+from squirrel.store import SquirrelStore
+
+if TYPE_CHECKING:
+    from squirrel.constants import ShardType
+    from squirrel.iterstream import Composable
+    from squirrel.store import AbstractStore
+
+@dataclass
+class SaveShardsConfig:
+    identifier: str  # identifier used in the catalog for the dataset
+    version: int  # dataset version to use
+    num_shards: int
+    output_data_url: str  # path where the shard will be written
+    output_catalog_url: str  # path where the catalog will be written
+    num_samples: Optional[int] = None  # number of samples to take from the dataset, if None, all samples will be taken
+
+
+def save_iterable_as_shard(shard: ShardType, store: AbstractStore) -> None:
+    """Helper to save a single shard into a messagepack store using squirrel"""
+    store.set(value=list(shard))
+
+
+def save_composable_to_shards(
+    src_it: Composable,
+    session: SparkSession,
+    out_url: str,
+    num_shards: int,
+    num_samples: int = None,
+    hooks: Optional[List[Callable[[Dict], Dict]]] = None,
+    save_catalog: Optional[bool] = False,
+    catalog_identifier: Optional[str] = "",
+    catalog_version: Optional[int] = 1,
+    catalog_output_url: Optional[str] = "",
+) -> None:
+    """Save single iterstream to messagepack.
+    
+    Args:
+        src_it (Composable): Composable to fetch data from.
+        session (SparkSession): Spark session to use for processing.
+        out_url (str): Url where to store data.
+        num_shards (int): Number of msgpack shards to create.
+        num_samples (int): Optionally store only n samples from the composable.
+        hooks (Optional[List[Callable[[Dict], Dict]]]): Methods to map to the samples before creating shards.
+        save_catalog (Optional[bool]): Optionally save the created dataset to a squirrel catalog.
+        catalog_identifier (Optional[str]): Dataset identifier for squirrel catalog.
+        catalog_version (Optional[int]): Dataset version for squirrel catalog.
+        catalog_output_url (Optional[str]): Output url where to store the squirrel catalog.
+    """
+    if num_samples is not None:
+        src_it = src_it.take(num_samples)
+
+    store = SquirrelStore(out_url, serializer=MessagepackSerializer())
+    pipe = session.sparkContext.parallelize(src_it)
+    for h in hooks:
+        pipe = pipe.map(h)
+    pipe = pipe.repartition(num_shards)
+    _ = pipe.foreachPartition(partial(save_iterable_as_shard, store=store))
+
+    if save_catalog:
+        ncat = Catalog()
+        # set version via DummyCatalogSource
+        cat_source = ncat[catalog_identifier]
+        cat_source[catalog_version + 1] = Source(
+            driver_name="messagepack",
+            driver_kwargs={"url": out_url},
+        )
+        ncat.to_file(catalog_output_url)
+
+
+def save_source_to_shards(
+    cfg: SaveShardsConfig,
+    session: SparkSession,
+    iter_kwargs: Dict,
+    hooks: Optional[List[Callable[[Dict], Dict]]] = None,
+) -> None:
+    """Process a source and save it as messagepack.
+
+    Args:
+        cfg (SaveShardsConfig): Config to use.
+        session (SparkSession): Spark session.
+        iter_kwargs (Dict): Keyword arguments passed to :py:meth:`Driver.get_iter`.
+        hooks (Optional[List[Callable[[Dict], Dict]]], optional): Methods to map to the samples before creating shards.
+    """
+    if hooks is None:
+        hooks = []
+
+    # get raw data
+    cat = Catalog.from_plugins()
+    d = cat[cfg.identifier][cfg.version]
+    src_it = d.load.get_iter(**iter_kwargs)
+
+    save_composable_to_shards(src_it, session, cfg.output_data_url, cfg.num_samples, cfg.num_shards, hooks,
+                              save_catalog=True, catalog_identifier=cfg.identifier, catalog_version=cfg.version, 
+                              catalog_output_url=cfg.output_catalog_url)
---
+++ src/squirrel_datasets_core/spark/setup_spark.py
@@ -0,0 +1,107 @@
+import os
+import typing as t
+
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+
+
+def get_spark(
+    app_name: str,
+    dataset_conf: t.Dict[str, str] = None,
+    add_gcs_connector: bool = True,
+    add_hadoop: bool = False,
+    mem_size: str = "4g",
+    n_master_cores: int = None
+) -> SparkSession:
+    """
+    Get a spark session for your operation.
+
+    Args:
+        app_name: The name of your spark session.
+        dataset_conf: A dictionary containing configs for your runtime spark configs. Must be configs that can be
+            altered after the creation of the spark session, otherwise will be complained by spark. A detailed list of
+            viable options can be found here: https://spark.apache.org/docs/latest/configuration.html.
+        add_gcs_connector: If true, will add shaded gcs-connector for hadoop3 and other related jar files to spark.
+            It must be True in order for cloudbuild to work.
+        add_hadoop: If true, will add hadoop native libs to spark lib path, such that spark can use native hadoop lib
+            tools to compress and decompress zst files.
+        mem_size: Memory size for the driver node.
+        n_master_cores: Specify number of parallel threads directly.
+
+    Returns:
+        SparkSession
+    """
+    conf = SparkConf()
+    conf.set("spark.executor.memory", mem_size)
+    conf.set("spark.jars.repositories", "https://maven.google.com/")  # add google maven repo as extra jars lookup link.
+    # add already installed jar files into path.
+    _SPARK_HOME = _get_home("SPARK_HOME")
+    _EXTRA_LIB_PATH = f"{_SPARK_HOME}/jars"
+    conf.set("spark.driver.extraLibraryPath", _EXTRA_LIB_PATH)
+    conf.set("spark.executor.extraLibraryPath", _EXTRA_LIB_PATH)
+
+    if add_gcs_connector:
+        conf = _add_gcs_connector(conf)
+
+    if add_hadoop:
+        conf = _add_native_hadoop(conf, extra_lib_path=_EXTRA_LIB_PATH)
+
+    assert isinstance(app_name, str), ValueError("`app_name` accept string only.")
+
+    if n_master_cores is not None:
+        spark = SparkSession.builder.master(f"local[{n_master_cores}]").appName(app_name).config(conf=conf).getOrCreate()
+    else:
+        spark = SparkSession.builder.appName(app_name).config(conf=conf).getOrCreate()
+
+    if dataset_conf:
+        for key, value in dataset_conf.items():
+            spark.conf.set(key, str(value))
+
+    return spark
+
+
+def _get_home(env_var: str) -> str:
+    """Get home directories for e.g. spark, hadoop or other java lib homes."""
+    _HOME = os.environ.get(env_var)
+    assert _HOME is not None, EnvironmentError(f"Could not find '{env_var}' in current OS env.")
+    return _HOME
+
+
+def _add_gcs_connector(conf: SparkConf) -> SparkConf:
+    """Add gcs-connector and related dependencies (jar files) to spark configuration."""
+    _SPARK_HOME = _get_home("SPARK_HOME")
+    GCS_CONNECTOR_JARS = {
+        "spark.jars.packages": "com.google.cloud.bigdataoss:gcs-connector:hadoop3-2.2.2",
+        "spark.hadoop.fs.AbstractFileSystem.gs.impl": "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS",
+        "spark.hadoop.fs.gs.impl": "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem",
+        "spark.hadoop.google.cloud.auth.service.account.enable": "true",
+        "spark.jars": f"{_SPARK_HOME}/jars/gcs-connector-hadoop3-latest.jar",
+    }
+    for key, val in GCS_CONNECTOR_JARS.items():
+        conf.set(key, val)
+    return conf
+
+
+def _add_native_hadoop(conf: SparkConf, extra_lib_path: str = None) -> SparkConf:
+    """Link extra hadoop native libs to spark configuration, such that spark can use these libraries to do some specific
+    IO works. Must be added when one does I/O with zstandard compressed files in spark. (The actual hadoop native libs
+    are pre-installed in docker image `infrastructure/docker/Dockerfile.spark`.
+    """
+    _HADOOP_HOME = _get_home("HADOOP_HOME")
+    if extra_lib_path is not None:
+        extra_lib_path = f"{extra_lib_path}:{_HADOOP_HOME}/lib/native"
+    else:
+        extra_lib_path = f"{_HADOOP_HOME}/lib/native"
+    conf.set("spark.driver.extraLibraryPath", extra_lib_path)
+    conf.set("spark.executor.extraLibraryPath", extra_lib_path)
+    return conf
+
+
+def stop_spark(spark: SparkSession) -> None:
+    """Stop a spark session in a graceful way."""
+    spark.sparkContext._gateway.shutdown_callback_server()
+    spark.stop()
+
+
+if __name__ == "__main__":
+    spark = get_spark("test")
