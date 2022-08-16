fd976ddec85fa6dabd5a8c284a7c7182940af9de
Winfried Loetzsch <winfried.loetzsch@merantix.com>
2022-08-16 12:25:06 +0200

    Add catalog entries for all sub-datasets

--- src/squirrel_datasets_core/datasets/fem_simulations/__init__.py
+++ src/squirrel_datasets_core/datasets/fem_simulations/__init__.py
@@ -6,17 +6,111 @@ __all__ = ["SOURCES"]
 
 SOURCES = [
     (
-        CatalogKey("ElectrostaticsFemData", 1),
+        CatalogKey("GnnBvpEsTrainNoMa", 1),
         Source(
-            driver_name="MessagepackDriver",
-            driver_kwargs={"url": "gs://squirrel-core-public-data/gnn_bvp_solver/ElectricsRandomChargeGenerator"},
+            driver_name="messagepack",
+            driver_kwargs={
+                "url": "gs://squirrel-core-public-data/gnn_bvp_solver/ElectricsRandomChargeGenerator/norm_train_no_ma"
+            },
         ),
     ),
     (
-        CatalogKey("MagnetostaticsFemData", 1),
+        CatalogKey("GnnBvpEsTrainMa", 1),
         Source(
-            driver_name="MessagepackDriver",
-            driver_kwargs={"url": "gs://squirrel-core-public-data/gnn_bvp_solver/MagneticsRandomCurrentGenerator"},
+            driver_name="messagepack",
+            driver_kwargs={
+                "url": "gs://squirrel-core-public-data/gnn_bvp_solver/ElectricsRandomChargeGenerator/norm_train_ma"
+            },
+        ),
+    ),
+    (
+        CatalogKey("GnnBvpEsValNoMa", 1),
+        Source(
+            driver_name="messagepack",
+            driver_kwargs={
+                "url": "gs://squirrel-core-public-data/gnn_bvp_solver/ElectricsRandomChargeGenerator/norm_val_no_ma"
+            },
+        ),
+    ),
+    (
+        CatalogKey("GnnBvpEsValMa", 1),
+        Source(
+            driver_name="messagepack",
+            driver_kwargs={
+                "url": "gs://squirrel-core-public-data/gnn_bvp_solver/ElectricsRandomChargeGenerator/norm_val_ma"
+            },
+        ),
+    ),
+    (
+        CatalogKey("GnnBvpEsTestShape", 1),
+        Source(
+            driver_name="messagepack",
+            driver_kwargs={
+                "url": "gs://squirrel-core-public-data/gnn_bvp_solver/ElectricsRandomChargeGenerator/norm_test_shape"
+            },
+        ),
+    ),
+    (
+        CatalogKey("GnnBvpEsTestSup", 1),
+        Source(
+            driver_name="messagepack",
+            driver_kwargs={
+                "url": "gs://squirrel-core-public-data/gnn_bvp_solver/ElectricsRandomChargeGenerator/norm_test_sup"
+            },
+        ),
+    ),
+    (
+        CatalogKey("GnnBvpMsTrainNoMa", 1),
+        Source(
+            driver_name="messagepack",
+            driver_kwargs={
+                "url": "gs://squirrel-core-public-data/gnn_bvp_solver/MagneticsRandomCurrentGenerator/norm_train_no_ma"
+            },
+        ),
+    ),
+    (
+        CatalogKey("GnnBvpMsTrainMa", 1),
+        Source(
+            driver_name="messagepack",
+            driver_kwargs={
+                "url": "gs://squirrel-core-public-data/gnn_bvp_solver/MagneticsRandomCurrentGenerator/norm_train_ma"
+            },
+        ),
+    ),
+    (
+        CatalogKey("GnnBvpMsValNoMa", 1),
+        Source(
+            driver_name="messagepack",
+            driver_kwargs={
+                "url": "gs://squirrel-core-public-data/gnn_bvp_solver/MagneticsRandomCurrentGenerator/norm_val_no_ma"
+            },
+        ),
+    ),
+    (
+        CatalogKey("GnnBvpMsValMa", 1),
+        Source(
+            driver_name="messagepack",
+            driver_kwargs={
+                "url": "gs://squirrel-core-public-data/gnn_bvp_solver/MagneticsRandomCurrentGenerator/norm_val_ma"
+            },
+        ),
+    ),
+    (
+        CatalogKey("GnnBvpMsTestShape", 1),
+        Source(
+            driver_name="messagepack",
+            driver_kwargs={
+                "url": "gs://squirrel-core-public-data/gnn_bvp_solver/MagneticsRandomCurrentGenerator/norm_test_shape"
+            },
+        ),
+    ),
+    (
+        CatalogKey("GnnBvpMsTestSup", 1),
+        Source(
+            driver_name="messagepack",
+            driver_kwargs={
+                "url": "gs://squirrel-core-public-data/gnn_bvp_solver/MagneticsRandomCurrentGenerator/norm_test_sup"
+            },
         ),
     ),
 ]
