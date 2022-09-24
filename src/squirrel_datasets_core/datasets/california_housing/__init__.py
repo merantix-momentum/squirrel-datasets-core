bf246e90ee6e6fcf3ec0f4429d0d7c1092f81e43
Johannes Otterbach <johannes.otterbach@merantix.com>
2022-09-24 13:19:26 +0200

    Create california housing dataset

---
+++ src/squirrel_datasets_core/datasets/california_housing/__init__.py
@@ -0,0 +1,15 @@
+from squirrel.catalog import Source, CatalogKey
+from squirrel_datasets_core.datasets.california_housing.driver import CaliforniaHousing
+
+__all__ = ["SOURCES", "DRIVERS"]
+
+DRIVERS = [CaliforniaHousing]
+SOURCES = [
+    (
+        CatalogKey("california_housing", 1),
+        Source(
+            driver_name="california_housing",
+            metadata={"src": "https://github.com/scikit-learn/scikit-learn/blob/36958fb24/sklearn/datasets/_california_housing.py"},
+        ),
+    )
+]
---
+++ src/squirrel_datasets_core/datasets/california_housing/driver.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+import os
+import pandas as pd
+
+from squirrel.driver import IterDriver
+from squirrel.iterstream import IterableSource
+
+if TYPE_CHECKING:
+    from squirrel.iterstream import Composable
+
+META_DATA = dict(
+    filename="cal_housing.tgz",
+    url="https://ndownloader.figshare.com/files/5976036",
+)
+
+_FEATURE_NAMES = [
+    "longitude",
+    "latitude",
+    "housingMedianAge",
+    "totalRooms",
+    "totalBedrooms",
+    "population",
+    "households",
+    "medianIncome",
+    "medianHouseValue"
+]
+
+class CaliforniaHousing(IterDriver):
+
+    name = "california_housing"
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self._data = (pd.read_csv(os.path.join(META_DATA["url"], META_DATA["filename"]),
+                         compression="gzip", index_col=None, header=None, names=_FEATURE_NAMES)
+                         .to_dict(orient='records'))
+
+    def get_iter(
+        self,
+        shuffle_item_buffer: int = 100,
+         **kwargs) -> Composable:
+        return IterableSource(self._data).shuffle(size=shuffle_item_buffer)
