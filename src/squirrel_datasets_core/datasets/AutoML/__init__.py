a562760caff4095fb7e45e39d398ea7c4e069f53
Johannes Otterbach <johannes.otterbach@merantix.com>
2022-09-24 15:47:02 +0200

    Linting time

--- src/squirrel_datasets_core/datasets/AutoML/__init__.py
+++ src/squirrel_datasets_core/datasets/AutoML/__init__.py
@@ -9,21 +9,16 @@ SOURCES = [
         CatalogKey("helena", 1),
         Source(
             driver_name="automl",
-            metadata={
-                "src": "https://automl.chalearn.org/data"
-            },
+            metadata={"src": "https://automl.chalearn.org/data"},
             driver_kwargs={"dataset_name": "helena"},
-            
         ),
     ),
     (
         CatalogKey("jannis", 1),
         Source(
             driver_name="automl",
-            metadata={
-                "src": "https://automl.chalearn.org/data"
-            },
+            metadata={"src": "https://automl.chalearn.org/data"},
             driver_kwargs={"dataset_name": "jannis"},
         ),
-    )
+    ),
 ]
--- src/squirrel_datasets_core/datasets/AutoML/driver.py
+++ src/squirrel_datasets_core/datasets/AutoML/driver.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, List, Dict, Any
 import io
 import zipfile
 import requests
@@ -12,8 +12,8 @@ if TYPE_CHECKING:
     from squirrel.iterstream import Composable
 
 URLS = {
-    'helena': "https://competitions.codalab.org/my/datasets/download/09ada795-4052-4fac-957a-87f02229b201",
-    'jannis': "http://www.causality.inf.ethz.ch/AutoML/jannis.zip"
+    "helena": "https://competitions.codalab.org/my/datasets/download/09ada795-4052-4fac-957a-87f02229b201",
+    "jannis": "http://www.causality.inf.ethz.ch/AutoML/jannis.zip",
 }
 
 
@@ -21,53 +21,51 @@ class AutoML(IterDriver):
 
     name = "automl"
 
-    def __init__(self, dataset_name: str, split:str = 'train', **kwargs) -> None:
+    def __init__(self, dataset_name: str, split: str = "train", **kwargs) -> None:
         """Initialze the Helena dataset driver."""
         super().__init__(**kwargs)
-        self.dataset_name=dataset_name
+        self.dataset_name = dataset_name
         self.split = split
         self.zipfile = zipfile.ZipFile(io.BytesIO(requests.get(URLS[self.dataset_name]).content))
-        if self.split == 'train':
+        if self.split == "train":
             self._data = self._get_train_split()
         else:
             self._data = self._get_test_or_valid_split()
-    
-    def _get_train_split(self):
+
+    def _get_train_split(self) -> List[Dict[str, Any]]:
         """Create train split"""
         records = []
-        for feat, lbl in zip(self.zipfile.read(f'{self.dataset_name}_train.data').decode('utf-8').split('\n'),
-                             self.zipfile.read(f'{self.dataset_name}_train.solution').decode('utf-8').split('\n')):
+        for feat, lbl in zip(
+            self.zipfile.read(f"{self.dataset_name}_train.data").decode("utf-8").split("\n"),
+            self.zipfile.read(f"{self.dataset_name}_train.solution").decode("utf-8").split("\n"),
+        ):
             try:
-                records.append({
-                    'features': [float(x) for x in feat.strip().split(" ")],
-                    'class': int(np.argwhere(np.asarray([int(x) for x in lbl.strip().split(" ")]) == 1))
-                })
-            except ValueError as e:
+                records.append(
+                    {
+                        "features": [float(x) for x in feat.strip().split(" ")],
+                        "class": int(np.argwhere(np.asarray([int(x) for x in lbl.strip().split(" ")]) == 1)),
+                    }
+                )
+            except ValueError:
                 pass
         return records
 
-    def _get_test_or_valid_split(self):
-        """
-        Create test or validation split
-        """
-
+    def _get_test_or_valid_split(self) -> List[Dict[str, Any]]:
+        """Create test or validation split"""
         records = []
-        for feat in self.zipfile.read(f'{self.dataset_name}_{self.split}.data').decode('utf-8').split('\n'):
+        for feat in self.zipfile.read(f"{self.dataset_name}_{self.split}.data").decode("utf-8").split("\n"):
             try:
-                records.append({
-                    'features': [float(x) for x in feat.strip().split(" ")],
-                    'class': None
-                })
-            except ValueError as e:
+                records.append({"features": [float(x) for x in feat.strip().split(" ")], "class": None})
+            except ValueError:
                 pass
         return records
-
 
     def get_iter(self, shuffle_item_buffer: int = 100, **kwargs) -> Composable:
         """
         Get an iterator over samples.
 
         Args:
-            shuffle_item_buffer (int): the size of the buffer used to shuffle samples after being fetched. Please note the memory footprint of samples
+            shuffle_item_buffer (int): the size of the buffer used to shuffle samples after being fetched.
+                Please note the memory footprint of samples
         """
         return IterableSource(self._data).shuffle(size=shuffle_item_buffer)
