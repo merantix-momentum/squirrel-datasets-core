57cfb71876148fb445e761c548500ca908706c28
Alexander Koenig <awckoenig@gmail.com>
2022-10-31 16:20:52 +0100

    Add driver docs

---
+++ docs/code/all_drivers.py
@@ -0,0 +1,27 @@
+from squirrel_datasets_core.driver import (
+    HubDriver,
+    HuggingfaceDriver,
+    TorchvisionDriver,
+)
+
+HuggingfaceDriver("cifar100").get_iter("train").take(1).map(print).join()
+# prints
+# {
+#     "img": <PIL.PngImagePlugin.PngImageFile image mode=RGB size=32x32 at 0x1424D6310>,
+#     "fine_label": 19,
+#     "coarse_label": 11,
+# }
+
+HubDriver("hub://activeloop/cifar100-train").get_iter().take(1).map(print).join()
+# prints
+# {
+#     "images": Tensor(key="images", index=Index([0])),
+#     "labels": Tensor(key="labels", index=Index([0])),
+# }
+
+TorchvisionDriver("cifar100", download=True).get_iter().take(1).map(print).join()
+# prints
+# (
+#     <PIL.Image.Image image mode=RGB size=32x32 at 0x143BD77C0>,
+#     6,
+# )
