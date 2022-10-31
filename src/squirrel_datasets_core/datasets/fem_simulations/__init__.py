6f6bff8277f3ece9ea46fcd203ce0ee40039d1d0
Winfried Ripken <winfried.loetzsch@merantix.com>
2022-10-31 13:43:59 +0100

    Add elasticity dataset (#62)
    * Add elasticity data
    
    * Register documentation
    
    * Avoid redundant metadata strings

--- src/squirrel_datasets_core/datasets/fem_simulations/__init__.py
+++ src/squirrel_datasets_core/datasets/fem_simulations/__init__.py
@@ -1,126 +1,188 @@
 from squirrel.catalog import CatalogKey, Source
 
 __all__ = ["SOURCES"]
+DRIVER_NAME = "messagepack"
+METADATA = {"publisher": "Merantix Momentum", "paper url": "https://arxiv.org/abs/2206.14092"}
 
 SOURCES = [
     (
         CatalogKey("GnnBvpEsTrainNoMa", 1),
         Source(
-            driver_name="messagepack",
+            driver_name=DRIVER_NAME,
             driver_kwargs={
                 "url": "gs://squirrel-core-public-data/gnn_bvp_solver/ElectricsRandomChargeGenerator/norm_train_no_ma"
             },
-            metadata={"publisher": "Merantix Momentum", "paper url": "https://arxiv.org/abs/2206.14092"},
+            metadata=METADATA,
         ),
     ),
     (
         CatalogKey("GnnBvpEsTrainMa", 1),
         Source(
-            driver_name="messagepack",
+            driver_name=DRIVER_NAME,
             driver_kwargs={
                 "url": "gs://squirrel-core-public-data/gnn_bvp_solver/ElectricsRandomChargeGenerator/norm_train_ma"
             },
-            metadata={"publisher": "Merantix Momentum", "paper url": "https://arxiv.org/abs/2206.14092"},
+            metadata=METADATA,
         ),
     ),
     (
         CatalogKey("GnnBvpEsValNoMa", 1),
         Source(
-            driver_name="messagepack",
+            driver_name=DRIVER_NAME,
             driver_kwargs={
                 "url": "gs://squirrel-core-public-data/gnn_bvp_solver/ElectricsRandomChargeGenerator/norm_val_no_ma"
             },
-            metadata={"publisher": "Merantix Momentum", "paper url": "https://arxiv.org/abs/2206.14092"},
+            metadata=METADATA,
         ),
     ),
     (
         CatalogKey("GnnBvpEsValMa", 1),
         Source(
-            driver_name="messagepack",
+            driver_name=DRIVER_NAME,
             driver_kwargs={
                 "url": "gs://squirrel-core-public-data/gnn_bvp_solver/ElectricsRandomChargeGenerator/norm_val_ma"
             },
-            metadata={"publisher": "Merantix Momentum", "paper url": "https://arxiv.org/abs/2206.14092"},
+            metadata=METADATA,
         ),
     ),
     (
         CatalogKey("GnnBvpEsTestShape", 1),
         Source(
-            driver_name="messagepack",
+            driver_name=DRIVER_NAME,
             driver_kwargs={
                 "url": "gs://squirrel-core-public-data/gnn_bvp_solver/ElectricsRandomChargeGenerator/norm_test_shape"
             },
-            metadata={"publisher": "Merantix Momentum", "paper url": "https://arxiv.org/abs/2206.14092"},
+            metadata=METADATA,
         ),
     ),
     (
         CatalogKey("GnnBvpEsTestSup", 1),
         Source(
-            driver_name="messagepack",
+            driver_name=DRIVER_NAME,
             driver_kwargs={
                 "url": "gs://squirrel-core-public-data/gnn_bvp_solver/ElectricsRandomChargeGenerator/norm_test_sup"
             },
-            metadata={"publisher": "Merantix Momentum", "paper url": "https://arxiv.org/abs/2206.14092"},
+            metadata=METADATA,
         ),
     ),
     (
         CatalogKey("GnnBvpMsTrainNoMa", 1),
         Source(
-            driver_name="messagepack",
+            driver_name=DRIVER_NAME,
             driver_kwargs={
                 "url": "gs://squirrel-core-public-data/gnn_bvp_solver/MagneticsRandomCurrentGenerator/norm_train_no_ma"
             },
-            metadata={"publisher": "Merantix Momentum", "paper url": "https://arxiv.org/abs/2206.14092"},
+            metadata=METADATA,
         ),
     ),
     (
         CatalogKey("GnnBvpMsTrainMa", 1),
         Source(
-            driver_name="messagepack",
+            driver_name=DRIVER_NAME,
             driver_kwargs={
                 "url": "gs://squirrel-core-public-data/gnn_bvp_solver/MagneticsRandomCurrentGenerator/norm_train_ma"
             },
-            metadata={"publisher": "Merantix Momentum", "paper url": "https://arxiv.org/abs/2206.14092"},
+            metadata=METADATA,
         ),
     ),
     (
         CatalogKey("GnnBvpMsValNoMa", 1),
         Source(
-            driver_name="messagepack",
+            driver_name=DRIVER_NAME,
             driver_kwargs={
                 "url": "gs://squirrel-core-public-data/gnn_bvp_solver/MagneticsRandomCurrentGenerator/norm_val_no_ma"
             },
-            metadata={"publisher": "Merantix Momentum", "paper url": "https://arxiv.org/abs/2206.14092"},
+            metadata=METADATA,
         ),
     ),
     (
         CatalogKey("GnnBvpMsValMa", 1),
         Source(
-            driver_name="messagepack",
+            driver_name=DRIVER_NAME,
             driver_kwargs={
                 "url": "gs://squirrel-core-public-data/gnn_bvp_solver/MagneticsRandomCurrentGenerator/norm_val_ma"
             },
-            metadata={"publisher": "Merantix Momentum", "paper url": "https://arxiv.org/abs/2206.14092"},
+            metadata=METADATA,
         ),
     ),
     (
         CatalogKey("GnnBvpMsTestShape", 1),
         Source(
-            driver_name="messagepack",
+            driver_name=DRIVER_NAME,
             driver_kwargs={
                 "url": "gs://squirrel-core-public-data/gnn_bvp_solver/MagneticsRandomCurrentGenerator/norm_test_shape"
             },
-            metadata={"publisher": "Merantix Momentum", "paper url": "https://arxiv.org/abs/2206.14092"},
+            metadata=METADATA,
         ),
     ),
     (
         CatalogKey("GnnBvpMsTestSup", 1),
         Source(
-            driver_name="messagepack",
+            driver_name=DRIVER_NAME,
             driver_kwargs={
                 "url": "gs://squirrel-core-public-data/gnn_bvp_solver/MagneticsRandomCurrentGenerator/norm_test_sup"
             },
-            metadata={"publisher": "Merantix Momentum", "paper url": "https://arxiv.org/abs/2206.14092"},
+            metadata=METADATA,
+        ),
+    ),
+    (
+        CatalogKey("GnnBvpElTrainNoMa", 1),
+        Source(
+            driver_name=DRIVER_NAME,
+            driver_kwargs={
+                "url": "gs://squirrel-core-public-data/gnn_bvp_solver/ElasticityFixedLineGenerator/norm_train_no_ma"
+            },
+            metadata=METADATA,
+        ),
+    ),
+    (
+        CatalogKey("GnnBvpElTrainMa", 1),
+        Source(
+            driver_name=DRIVER_NAME,
+            driver_kwargs={
+                "url": "gs://squirrel-core-public-data/gnn_bvp_solver/ElasticityFixedLineGenerator/norm_train_ma"
+            },
+            metadata=METADATA,
+        ),
+    ),
+    (
+        CatalogKey("GnnBvpElValNoMa", 1),
+        Source(
+            driver_name=DRIVER_NAME,
+            driver_kwargs={
+                "url": "gs://squirrel-core-public-data/gnn_bvp_solver/ElasticityFixedLineGenerator/norm_val_no_ma"
+            },
+            metadata=METADATA,
+        ),
+    ),
+    (
+        CatalogKey("GnnBvpElValMa", 1),
+        Source(
+            driver_name=DRIVER_NAME,
+            driver_kwargs={
+                "url": "gs://squirrel-core-public-data/gnn_bvp_solver/ElasticityFixedLineGenerator/norm_val_ma"
+            },
+            metadata=METADATA,
+        ),
+    ),
+    (
+        CatalogKey("GnnBvpElTestShape", 1),
+        Source(
+            driver_name=DRIVER_NAME,
+            driver_kwargs={
+                "url": "gs://squirrel-core-public-data/gnn_bvp_solver/ElasticityFixedLineGenerator/norm_test_shape"
+            },
+            metadata=METADATA,
+        ),
+    ),
+    (
+        CatalogKey("GnnBvpElTestSup", 1),
+        Source(
+            driver_name=DRIVER_NAME,
+            driver_kwargs={
+                "url": "gs://squirrel-core-public-data/gnn_bvp_solver/ElasticityFixedLineGenerator/norm_test_sup"
+            },
+            metadata=METADATA,
         ),
     ),
 ]
