875274b841db9128e65d205f48b177490fdd496d
Alexander Koenig <50868646+axkoenig@users.noreply.github.com>
2022-12-19 14:33:18 +0100

    Fix Driver imports (#74)
    * Fix driver imports
    
    * Lint
    
    * Version bump

--- src/squirrel_datasets_core/driver/__init__.py
+++
@@ -1,6 +0,0 @@
-from squirrel_datasets_core.driver.huggingface import HuggingfaceDriver
-from squirrel_datasets_core.driver.torchvision import TorchvisionDriver
-from squirrel_datasets_core.driver.hub import HubDriver
-from squirrel_datasets_core.driver.deeplake import DeeplakeDriver
-
-__all__ = ["HuggingfaceDriver", "TorchvisionDriver", "HubDriver", "DeeplakeDriver"]
