5739b17010be3f838411a7b6d6f4d5dabb09fcb8
maik-schmidt <110388332+maik-schmidt@users.noreply.github.com>
2023-02-18 11:59:43 +0100

    Use poetry-core for building the package

--- setup.py
+++ setup.py
@@ -2,19 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import ast
-import itertools
-import os
+import subprocess
 import re
 import sys
-from pathlib import Path
 
-from setuptools import find_packages, setup
 
 SOURCE_DIR = "src/squirrel_datasets_core"
 
 # Read package information from other files so that just one version has to be maintained.
-_version_re = re.compile(r"__version__\s+=\s+(.*)")
-with open("./%s/__init__.py" % SOURCE_DIR, "rb") as f:
+with open("pyproject.toml", "rb") as f:
     init_contents = f.read().decode("utf-8")
 
     def get_var(var_name: str) -> str:
@@ -23,7 +19,7 @@ with open("./%s/__init__.py" % SOURCE_DIR, "rb") as f:
         match = pattern.search(init_contents).group(1)
         return str(ast.literal_eval(match))
 
-    version = get_var("__version__")
+    version = get_var("version")
 
 
 def assert_version(ver: str) -> None:
@@ -63,78 +59,14 @@ if "--version_tag" in sys.argv:
     sys.argv.pop(v_idx)
 
 
-def parse_req(spec: str) -> str:
-    """Parse package name==version out of requirements file."""
-    if ";" in spec:
-        # remove restriction
-        spec, _ = [x.strip() for x in spec.split(";", 1)]
-    if "#" in spec:
-        # remove comment
-        spec = spec.strip().split("#")[0]
-    if "\\" in spec:
-        # remove line breaks
-        spec = spec.strip().split("\\")[0]
-    if "--hash=" in spec:
-        # remove line breaks
-        spec = spec.strip().split("--hash=")[0]
-    return spec
-
-
-if os.path.exists("requirements.in"):
-    with open("requirements.in") as fh:
-        requirements = [parse_req(r) for r in fh.read().replace("\\\n", " ").split("\n") if parse_req(r) != ""]
-else:
-    requirements = []
-
-# generate extras based on requirements files
-extras_require = dict()
-for a_extra in ["dev", "preprocessing", "torchvision", "hub", "deeplake", "huggingface"]:
-    req_file = f"requirements.{a_extra}.in"
-    if os.path.exists(req_file):
-        with open(req_file) as fh:
-            extras_require[a_extra] = [r for r in fh.read().split("\n") if ";" not in r]
-    else:
-        extras_require[a_extra] = []
-extras_require["all"] = list(itertools.chain.from_iterable(extras_require.values()))
-
-if os.path.exists("scripts"):
-    SCRIPTS = [os.path.join("scripts", a) for a in os.listdir("scripts")]
-else:
-    SCRIPTS = []
-
-ENTRY_POINTS = {"squirrel": ["squirrel_datasets_core = squirrel_datasets_core.squirrel_plugin"]}
-
-# read the contents of your README file
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
-
-# TODO remove after beta-testing phase
-classifiers = [
-    "Development Status :: 5 - Production/Stable",
-    "License :: OSI Approved :: Apache Software License",
-    "Programming Language :: Python :: 3.8",
-    "Typing :: Typed",
-]
-
-# Setup package using PIP
 if __name__ == "__main__":
-    setup(
-        name="squirrel_datasets_core",
-        version=version,
-        description="Squirrel public datasets collection",
-        long_description=long_description,
-        long_description_content_type="text/markdown",
-        author="Merantix Momentum",
-        license="Apache 2.0",
-        # Needed to make jinja work and not get linting errors in the rendered file
-        package_dir={"": "src"},
-        packages=find_packages(where="src"),
-        scripts=SCRIPTS,
-        include_package_data=True,
-        install_requires=requirements,
-        tests_require=extras_require["dev"],
-        extras_require=extras_require,
-        entry_points=ENTRY_POINTS,
-        classifiers=classifiers,
-        package_data={"": ["*.rst"]},
+    # Kinda hacky way to inject our modified package version to the build process.
+    subprocess.run(
+        (
+            f"sed -r -i.bak 's/^version.*$/version = \"{version}\"/' pyproject.toml && "
+            "python -m build && "
+            "mv pyproject.toml.bak pyproject.toml"
+        ),
+        shell=True,
+        check=True,
     )
